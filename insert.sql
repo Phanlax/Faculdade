40e58b7255df506bef9f77c410d39a1bb94d0433
Phanlax <gustavo.os170205@gmail.com>
2025-05-05 16:50:19 -0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,8 +1,11 @@
-insert into clientes values 
-(211416411,'Miguel','miguel@gmail.com','36734746','rua claraboia')
+insert into clientes (rg, nome, email, telefone, endereço) 
+values
+(211416411,'Miguel','miguel@gmail.com','36734746','rua claraboia'),
 (943454456,'Marcos Santos','Marcos@gmail.com' , '(11) 98987-5252','Rua Castelo Branco, 10'),
 (534656547,'Carlos Silva','carlos@hotmail.com.br','(11) 3251-2587','Avenida Paulista, 1500'),
 (577425466,'Maria Souza','maria@outlook.com', '(21) 2507-8963','Rua da Liberdade, 554'),
 (466778890,'João Silva','joao@gmail.com','(31) 3571-4285' ,'Rua das Flores, 875');
 
-
+insert into pedido (id_pedido,data_pedido,status)
+values
+(5497929,'2025-05-05','em andamento')
