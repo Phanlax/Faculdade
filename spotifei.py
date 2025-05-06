681e56f60ed2af021d73eb68fffd947043d14cc1
Phanlax <gustavo.os170205@gmail.com>
2025-05-05 21:18:07 -0300

    Add files via upload

---
+++ spotifei.py
@@ -0,0 +1,102 @@
+def menu_inicial():
+    while True:
+        print("\n=== Bem-vindo ao Spotifei ===")
+        print("1. Login")
+        print("2. Cadastrar")
+        print("3. Sair")
+        opcao = input("Escolha uma opção: ")
+        if opcao == "1":
+            usuario = login()
+            if usuario:
+                menu_principal(usuario)
+        elif opcao == "2":
+            cadastro()
+        elif opcao == "3":
+            print("Obrigado por usar o Spotifei!")
+            break
+        else:
+            print("Opção inválida.")
+
+usuarios = []     
+ 
+def cadastro():
+    print("\n=== Cadastro de Usuário ===")
+    email = input("Email: ")
+    nome = input("digite seu nome de usuario: ")
+    for u in usuarios:
+        if u["email"] == email:
+            print("Email já cadastrado!")
+            return
+    senha = input("Senha: ")
+    usuarios.append({"email": email, "senha": senha, "nome": nome})
+    print("Cadastro realizado com sucesso!")
+
+def login():
+    print("\n=== Login ===")
+    email = input("Email: ")
+    senha = input("Senha: ")
+    for u in usuarios:
+        if u["email"] == email and u["senha"] == senha:
+            print(f"Bem-vindo(a), {email}!") #ainda não sei como salvar o nome / ver depois
+            return u
+    print("Credenciais inválidas.")
+    return None
+
+def menu_principal(usuario):
+    while True:
+        print("\n=== Menu Spotifei ===")
+        print("1. Buscar músicas")
+        print("2. Ver músicas curtidas")
+        print("3. sair")
+        opcao = input("Escolha uma opção: ")
+        if opcao == "1":
+            buscar_musicas(usuario)
+        elif opcao == "2":
+            listar_curtidas(usuario)
+        elif opcao == "3":
+            print("Logout realizado.") #fazer sair direto
+            break
+        else:
+            print("Opção inválida.")
+             
+def buscar_musicas(usuario):
+    while True:
+        nome = input("\n Digite o nome da musica: ").lower() 
+        for m in musicas:
+             if nome in m["titulo"].lower():
+                print(f"[{m['id']}] {m['titulo']} - {m['artista']} ({m['genero']}, {m['duracao']})")
+                print("1-Curtir Musica")
+                print("2-Descurtir Musica")
+                print("3-Voltar")
+                a = input("escolha uma opção: ")
+             if a == "1":
+                 print("musica curtida.")
+             elif a == "2":
+                 print("musica descurtida.")
+             elif a == "3":
+                 return menu_principal(usuario)        
+        else:
+            print("Musica não encontrada")
+
+def listar_curtidas(usuario):
+    while True:
+        sla = input
+
+
+
+
+
+
+
+
+
+musicas = [
+    {"id": 1, "titulo": "Imagine", "artista": "John Lennon", "genero": "Rock", "duracao": "3:04"},
+    {"id": 2, "titulo": "Bohemian Rhapsody", "artista": "Queen", "genero": "Rock", "duracao": "5:55"},
+    {"id": 3, "titulo": "Shape of You", "artista": "Ed Sheeran", "genero": "Pop", "duracao": "4:24"},
+    {"id": 4, "titulo": "Blinding Lights", "artista": "The Weeknd", "genero": "Synthpop", "duracao": "3:20"},
+]
+
+
+
+menu_inicial()  
