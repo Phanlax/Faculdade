12fdedccdb93261f27b88c7fed6523c352289e0a
Phanlax <gustavo.os170205@gmail.com>
2025-05-07 11:40:38 -0300

    Update spotifeiM.py

--- spotifeiM.py
+++ spotifeiM.py
@@ -20,7 +20,6 @@ def menu_inicial():
         else:
             print("Opção inválida.")
 
-usuarios = []     
  
 def cadastro():
     print("\n=== Cadastro de Usuário ===")
@@ -42,21 +41,23 @@ def cadastro():
 
 def login():
     print("\n=== Login ===")
-    email = input("Email: ").lower()
+    email = input("Email: ")
     senha = input("Senha: ")
     with open("cadastros.txt", "r") as arquivo:
         for linha in arquivo:
-            print(f"Bem-vindo(a), {email}!") #ainda não sei como salvar o nome / ver depois
-            menu_principal()
-        else:
-            print("Credenciais inválidas.")
-    
+            if f"Email: {email}" in linha and f"Senha: {senha}" in linha:
+                print(f"Bem-vindo(a), {email}!")
+                return email  
+    print("Credenciais inválidas.")
+    return None
 
 def menu_principal(usuario):
     while True:
         print("\n=== Menu Spotifei ===")
         print("1. Buscar músicas")
         print("2. Ver músicas curtidas")
+        print("3. Gerenciar Playlist")
+        print("4. Ver histórico")
         print("3. sair")
         opcao = input("Escolha uma opção: ")
         if opcao == "1":
@@ -64,7 +65,7 @@ def menu_principal(usuario):
         elif opcao == "2":
             listar_curtidas(usuario)
         elif opcao == "3":
-            print("Logout realizado.") #fazer sair direto
+            print("Logout realizado.") 
             break
         else:
             print("Opção inválida.")
@@ -74,13 +75,16 @@ def buscar_musicas(usuario):
         nome = input("\n Digite o nome da musica: ").lower() 
         for m in musicas:
              if nome in m["titulo"].lower():
-                print(f"[{m['id']}] {m['titulo']} - {m['artista']} ({m['genero']}, {m['duracao']})")
+                print(f"{m['titulo']} - {m['artista']} ({m['genero']}, {m['duracao']})")
                 print("1-Curtir Musica")
                 print("2-Descurtir Musica")
                 print("3-Voltar")
                 a = input("escolha uma opção: ")
              if a == "1":
                  print("musica curtida.")
+                 open ("musicas_curtidas.txt", "+r")
+
+
              elif a == "2":
                  print("musica descurtida.")
              elif a == "3":
@@ -92,6 +96,9 @@ def listar_curtidas(usuario):
     while True:
         sla = input
 
+
+def playslist():
+    ...
 
 
 
