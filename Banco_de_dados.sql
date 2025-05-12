70d2f40e91ba46b5260acd319ad1a204b9d2a09a
Phanlax <gustavo.os170205@gmail.com>
2025-05-12 16:52:51 -0300

    Update Banco_de_dados.sql

--- Banco_de_dados.sql
+++ Banco_de_dados.sql
@@ -1,5 +1,14 @@
+drop table if exists clientes cascade;
+drop table if exists pedido cascade;
+drop table if exists entrega cascade;
+drop table if exists itens_pedido cascade;
+drop table if exists produtos cascade;
+drop table if exists fornecedor cascade;
+drop table if exists estoque cascade;
+drop table if exists transportadora cascade;
+
 create table Clientes (
-  Rg integer  primary key,
+  rg serial  primary key,
   Nome varchar(50) not null,
   email varchar(50) not null,
   telefone char(20) not null,
@@ -7,27 +16,33 @@ create table Clientes (
 );
 
 create table Pedido (
-  ID_Pedido integer primary key,
+  id_pedido integer primary key,
   Data_pedido timestamp without time zone not null ,
-  Status varchar(50) not null
+  status varchar(50) not null,
+  rg int ,
+  foreign key (rg) references clientes (rg)
 );
+
+create table transportadora (
+  id_transportadora integer primary key,
+  nome varchar(50) not null,
+  telefone char(20) not null,
+  tipo_transporte varchar(30) not null
+  );
 
 create table Entrega (
   ID_entrega integer primary key,
   Data_entrega timestamp without time zone not null ,
-  Status_entrega varchar(50) not null
+  Status_entrega varchar(50) not null,
+  id_transportadora int,
+  foreign key (id_transportadora) references transportadora(id_transportadora)
 );
 
 create table Itens_pedido (
   ID_item integer primary key,
-  Quantidade char(100) not null
-);
-
-create table produtos (
-  ID_produto integer primary key,
-  Nome_produto varchar(30) not null,
-  preço money not null,
-  quantidade_estoque integer not null
+  Quantidade char(100) not null,
+  id_pedido int,
+  foreign key (id_pedido) references pedido(id_pedido)
 );
 
 create table fornecedor (
@@ -43,9 +58,21 @@ create table estoque (
   localização varchar(30) not null
   );
 
-  create table transportadora(
-    ID_transportadora integer primary key,
-    nome varchar(50) not null,
-    telefone char(20) not null,
-    tipo_transporte varchar(30) not null
-  );
+create table produtos (
+  id_produto integer primary key,
+  Nome_produto varchar(100) not null,
+  preço money not null,
+  quantidade_estoque integer not null,
+  id_fornecedor int,
+  id_estoque int,
+  id_item int, 
+  foreign key (id_fornecedor) references fornecedor(id_fornecedor),
+  foreign key (id_estoque) references estoque(id_estoque),
+  foreign key (id_item) references itens_pedido(id_item)
+);
+
+
+
+
+
+  
