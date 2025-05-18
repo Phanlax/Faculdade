c27a67b102dc1366bff8f7a7fc197987ef983002
Phanlax <gustavo.os170205@gmail.com>
2025-05-18 19:07:18 -0300

    Update Banco_de_dados.sql

--- Banco_de_dados.sql
+++ Banco_de_dados.sql
@@ -6,43 +6,16 @@ drop table if exists produtos cascade;
 drop table if exists fornecedor cascade;
 drop table if exists estoque cascade;
 drop table if exists transportadora cascade;
+drop table if exists carga_especial cascade;
+drop table if exists carga_fragil cascade;
+drop table if exists carga_grande cascade;
 
-create table Clientes (
-  rg serial  primary key,
-  Nome varchar(50) not null,
+create table clientes (
+  rg varchar(12)  primary key,
+  nome varchar(50) not null,
   email varchar(50) not null,
   telefone char(20) not null,
-  endereço varchar(100) not null
-);
-
-create table Pedido (
-  id_pedido integer primary key,
-  Data_pedido timestamp without time zone not null ,
-  status varchar(50) not null,
-  rg int ,
-  foreign key (rg) references clientes (rg)
-);
-
-create table transportadora (
-  id_transportadora integer primary key,
-  nome varchar(50) not null,
-  telefone char(20) not null,
-  tipo_transporte varchar(30) not null
-  );
-
-create table Entrega (
-  ID_entrega integer primary key,
-  Data_entrega timestamp without time zone not null ,
-  Status_entrega varchar(50) not null,
-  id_transportadora int,
-  foreign key (id_transportadora) references transportadora(id_transportadora)
-);
-
-create table Itens_pedido (
-  ID_item integer primary key,
-  Quantidade char(100) not null,
-  id_pedido int,
-  foreign key (id_pedido) references pedido(id_pedido)
+  endereco varchar(100) not null
 );
 
 create table fornecedor (
@@ -53,25 +26,77 @@ create table fornecedor (
 );
 
 create table estoque (
-  ID_estoque integer primary key,
+  id_estoque integer primary key,
   Quantidade_estoque integer not null,
   localização varchar(30) not null
   );
 
 create table produtos (
   id_produto integer primary key,
-  Nome_produto varchar(100) not null,
+  nome_produto varchar(100) not null,
   preço money not null,
   quantidade_estoque integer not null,
   id_fornecedor int,
   id_estoque int,
-  id_item int, 
   foreign key (id_fornecedor) references fornecedor(id_fornecedor),
-  foreign key (id_estoque) references estoque(id_estoque),
-  foreign key (id_item) references itens_pedido(id_item)
+  foreign key (id_estoque) references estoque(id_estoque)
+);
+
+create table transportadora (
+  id_transportadora integer primary key,
+  nome varchar(50) not null,
+  telefone char(20) not null,
+  tipo_transporte varchar(30) not null
+  );
+
+
+
+create table entrega (
+    id_entrega int primary key,
+    data_entrega DATE,
+    status_entrega VARCHAR(50),
+    id_transportadora int,
+    tipo_carga VARCHAR(20), 
+    foreign key (ID_transportadora) references transportadora(id_transportadora)
+);
+
+create table pedido (
+  id_pedido int primary key,
+  data_pedido timestamp not null,
+  status varchar(50) not null,
+  rg varchar(12),
+  id_entrega int,
+  foreign key (rg) references clientes (rg),
+  foreign key (id_entrega) references entrega (id_entrega)
+);
+
+create table itens_pedido (
+  id_item int primary key,
+  quantidade int not null,
+  id_produto int,
+  id_pedido int,
+  foreign key (id_produto) references produtos(id_produto),
+  foreign key (id_pedido) references pedido(id_pedido)
 );
 
 
+create table carga_fragil (
+    id_entrega int primary key,
+    manuseio VARCHAR(100),
+    foreign key (ID_entrega) references entrega(ID_entrega)
+);
+
+create table carga_especial (
+    id_entrega int primary key,
+    cuidado VARCHAR(100),
+    foreign key (ID_entrega) references entrega(ID_entrega)
+);
+
+create table carga_grande (
+    id_entrega int primary key,
+    pessoas_necessarias int,
+    foreign key (ID_entrega) references entrega(ID_entrega)
+);
 
 
 
