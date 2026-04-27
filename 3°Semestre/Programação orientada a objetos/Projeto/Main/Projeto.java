da523236d3ebdeefc3dcb0ead800d7a633d8a95f
Gustavo de Oliveira Santos <gustavo.os170205@gmail.com>
2026-04-27 15:31:01 -0300

    Add files via upload

---
+++ 3°Semestre/Programação orientada a objetos/Projeto/Main/Projeto.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+import Controller.NavegacaoController;
+import View.TelaInicial;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Projeto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      new NavegacaoController().iniciar();
+       
+    }
+    
+}
