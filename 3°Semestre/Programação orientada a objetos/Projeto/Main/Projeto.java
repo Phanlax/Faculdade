3f2a7cd5d235be2d1f7072f0ae4a905c263fe37c
Gustavo de Oliveira Santos <gustavo.os170205@gmail.com>
2026-05-04 15:20:10 -0300

    Refactor Projeto class and update package structure

--- 3°Semestre/Programação orientada a objetos/Projeto/Main/Projeto.java
+++ 3°Semestre/Programação orientada a objetos/Projeto/Main/Projeto.java
@@ -2,23 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Main;
+package proj.projetofeitv;
 
-import Controller.NavegacaoController;
-import View.TelaInicial;
+import proj.projetofeitv.controller.ControllerNavegacao;
 
 /**
  *
  * @author Gustavo
  */
-public class Projeto {
+public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-      new NavegacaoController().iniciar();
-       
-    }
+     ControllerNavegacao controller = new ControllerNavegacao();
+        controller.iniciar();
+        
+    } 
     
 }
